d909c02363f1084727c74a798f06c37174e8c270
Mite <mite_mazgaliev@live.com>
2023-03-05 15:09:03 +0100

    Edit-item-modal bug fixes

--- FastFoodManagementBackend/src/main/java/com/example/fastfoodmanagmentbackend/Config/JWTAuthConstants.java
+++ FastFoodManagementBackend/src/main/java/com/example/fastfoodmanagmentbackend/Config/JWTAuthConstants.java
@@ -2,7 +2,7 @@ package com.example.fastfoodmanagmentbackend.Config;
 
 public class JWTAuthConstants {
     public static final long EXPIRATION_TIME = 64_000_000;
-    public static final String SECRET = "S3cr3tshitW1LdxD";
+    public static final String SECRET = "SomeSecret";
 
     public static final String HEADER_STRING = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
